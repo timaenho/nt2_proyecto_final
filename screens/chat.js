c249921f22deaa5c9ddad177b15e67f14a1d415c
tom maenhout <tom.maenhout@gmail.com>
2021-11-12 20:34:20 -0300

    chat-sockets

--- screens/chat.js
+++ screens/chat.js
@@ -5,13 +5,25 @@ import React, {useState, useContext,useEffect, useRef} from 'react';
 
 // 192.168.0.1
 // 172.20.176.1
+
 export default function App() {
 const[messageToSend, setMessageToSend] = useState("")
+const[recMessages, setRecMessages] =useState([])
 const socket = useRef(null)
-useEffect( () => {
-    console.log("in useEffect")
-    socket.current = io("https://172.20.176.1:3000")
-  },[])
+useEffect(() => {
+  socket.current = io("http://localhost:3000");
+  socket.current.on("message",message =>{
+    setRecMessages(prevState => [...prevState,message])
+  })
+  
+  return () => {
+    socket.current.disconnect()
+  }
+}, [])
+
+const textOfRecievedMessage = recMessages.map(msg => (
+<Text key ={msg}>{msg}</Text> 
+ ))
 
   const sendMessage = () => {
     socket.current.emit("message", messageToSend)
@@ -21,6 +33,7 @@ useEffect( () => {
 return (
     <View style={styles.container}>
        <Text>Chat</Text> 
+       {textOfRecievedMessage}
        <TextInput 
        value={messageToSend} 
        onChangeText={text => setMessageToSend(text)}
